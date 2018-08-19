cf7c678661a67b76fbea67299afbcb78a978329b
TENTACLEH3NTAI <philsterlp@gmail.com>
2018-08-19 16:02:11 +0200

    now with extra neko

---
+++ commands/nekop/neko_hug.js
@@ -0,0 +1,28 @@
+const commando = require("discord.js-commando");
+const neko = require("neko.js");
+let nekoclient = new neko.Client();
+
+class LewdCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'lewd',
+            group: 'nekop',
+            memberName: 'lewd',
+            description: 'Lewd nekos poggers'
+        });
+    }
+
+    async run(message, args)
+    {
+        if (message.channel.nsfw === false) {
+            nekoclient.hug().then((hug) => message.reply("take sum hugs instead"+hug.neko));
+        }
+        else{
+            nekoclient.LewdNeko().then((LewdNeko) => message.reply(LewdNeko.neko));
+        }
+    }
+}
+
+module.exports = LewdCommand;
