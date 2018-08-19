59e780296f367fdf86cf6f1fa011fb2c0013d2e6
TENTACLEH3NTAI <41243359+TENTACLEH3NTAI@users.noreply.github.com>
2018-08-19 15:36:17 +0200

    Update join_channel.js

--- commands/music/join_channel.js
+++ commands/music/join_channel.js
@@ -43,7 +43,7 @@ class JoinChannelCommand extends commando.Command
                 message.member.voiceChannel.join()
                     .then(connection =>{
                         var server = servers[message.guild.id];
-                        message.reply("Entered Channel! Now playing "+message);
+                        message.reply("Entered Channel! Now playing ");
                         server.queue.push(args);
                         Play(connection, message);
                     })
