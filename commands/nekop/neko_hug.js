e62cbe5e7176d9fe3d541e9794626ac75795529e
TENTACLEH3NTAI <41243359+TENTACLEH3NTAI@users.noreply.github.com>
2018-08-19 16:29:53 +0200

    Add files via upload

--- commands/nekop/neko_hug.js
+++ commands/nekop/neko_hug.js
@@ -1,5 +1,6 @@
 const commando = require("discord.js-commando");
 const neko = require("neko.js");
+const discord = require("discord.js");
 let nekoclient = new neko.Client();
 
 class LewdCommand extends commando.Command
@@ -16,8 +17,10 @@ class LewdCommand extends commando.Command
 
     async run(message, args)
     {
+        var lewdPic = new discord.RichEmbed()
+            .setDescription("Lewd Nekos provided by nekos.life")
         if (message.channel.nsfw === false) {
-            nekoclient.hug().then((hug) => message.reply('here, take some hugs instead:  '+hug.url));
+            nekoclient.hug().then((hug) => message.channel.sendEmbed(lewdPic)); //message.reply('here, take some hugs instead:  '+hug.url));
         }
         else{
             nekoclient.LewdNeko().then((LewdNeko) => message.reply(LewdNeko.neko));
