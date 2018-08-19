da3886a5ff701af1b10d8446650379fc24e631f1
TENTACLEH3NTAI <41243359+TENTACLEH3NTAI@users.noreply.github.com>
2018-08-19 18:37:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'NDc5Mjg1MzI0NTgxNjk5NjA1.DlnOYg.7TscrHuVIKE0ennm8sV4iNng55c';
 
-exports.PREFIX = 'don';
+exports.PREFIX = 'd';
 
 exports.GOOGLE_API_KEY = 'AIzaSyBztRBQ54yKmodgyjWZvjWdj-uECiCVL2A';
