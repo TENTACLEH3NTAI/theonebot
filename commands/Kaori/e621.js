7d51d842bbca519051562a3c44d197e3c959c577
TENTACLEH3NTAI <41243359+TENTACLEH3NTAI@users.noreply.github.com>
2018-09-09 00:54:23 +0200

    Add files via upload

---
+++ commands/Kaori/e621.js
@@ -0,0 +1,37 @@
+const commando = require("discord.js-commando");
+const Kaori = require("kaori");
+const kaori = new Kaori();
+const discord = require("discord.js");
+let nekoclient = new neko.Client();
+
+class LizardCommand extends commando.Command
+{
+    constructor(client)
+    {
+        super(client,{
+            name: 'cat',
+            group: 'Kaori',
+            memberName: 'cat',
+            description: 'end my life'
+        });
+    }
+
+    async run(message, args)
+    {
+		
+		kaori.search('danbooru', { tags: ['cat'], limit: 1, random: true })
+			.then(images => message.channel.sendMessage({
+            "embed": {
+                title: "nyan nyan!",
+                "image": {
+                "url": images[0].common.fileURL,
+                }
+				}
+			}))
+			.catch(err => console.error(err));
+	}
+}
+
+
+
+module.exports = LizardCommand;
